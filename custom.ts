f575ac13fd45727f30f7caef0550c36de61361cb
10-biter <ep.student@hotmail.com>
2025-11-23 14:57:44 +0300

    Update pxt.json, custom.ts

--- custom.ts
+++ custom.ts
@@ -1,8 +1,42 @@
 //% color=#424369
 
 namespace dofer {
-    //%block
+    //% groups="['gof', loo]"
+
+
+
+
+
+    //% block
+    //% group="gof"
     export function foo(i: string, sprite:Sprite): void {
-        sprite.sayText(i)
+        
     }
+
+
+
+
+
+    //% block
+    //% group="gof"
+    export function fig(i: string, sprite: Sprite): void {
+        
+    }
+
+
+    //% block
+    //% group="loo"
+    export function joo(i: string, sprite: Sprite): void {
+
+    }
+
+
+
+    //% block
+    //% group="loo"
+    export function koz(i: string, sprite: Sprite): void {
+
+    }
+
+
 }
